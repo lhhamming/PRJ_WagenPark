a4f984db6255afd34a63fe9d12dbae338ad43496
Luuk Hamming <luuk.hamming@gmail.com>
2018-05-30 12:38:26 +0200

    Added a way to login via username

--- WagenPark/Models/AccountViewModels.cs
+++ WagenPark/Models/AccountViewModels.cs
@@ -49,9 +49,8 @@ namespace WagenPark.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -64,10 +63,10 @@ namespace WagenPark.Models
 
     public class RegisterViewModel
     {
+
         [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
