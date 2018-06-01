138bb30c8e6b172d7e19f4feaee0c1c295d63a79
Luuk Hamming <luuk.hamming@gmail.com>
2018-06-01 11:28:00 +0200

    Added the DealerPage

---
+++ WagenPark/Controllers/DealerPageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WagenPark.Controllers
+{
+    public class DealerPageController : Controller
+    {
+        // GET: DealerPage
+        public ActionResult DealerPage()
+        {
+            return View();
+        }
+
+        public ActionResult Database()
+        {
+            return View();
+        }
+    }
+}
