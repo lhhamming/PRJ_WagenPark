b594ef7236697700a8a3206ca4c0088ec3f09495
Luuk Hamming <luuk.hamming@gmail.com>
2018-06-05 09:36:15 +0200

    Added auto dbd.

---
+++ WagenPark/Classes/Dealer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WagenPark.Classes
+{
+    public class Dealer
+    {
+        public string dealerNr { get; set; }
+        public string naam { get; set; }
+    }
+}
---
+++ WagenPark/Controllers/AutoesController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WagenPark.Classes;
+using WagenPark.Models;
+
+namespace WagenPark.Controllers
+{
+    [Authorize(Roles = "Dealer , Admin")]
+
+    public class AutoesController : Controller
+    {
+        private WagenParkContext db = new WagenParkContext();
+
+        // GET: Autoes
+        public ActionResult Index()
+        {
+            return View(db.Autoes.ToList());
+        }
+
+        // GET: Autoes/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Auto auto = db.Autoes.Find(id);
+            if (auto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(auto);
+        }
+
+        // GET: Autoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Autoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Kenteken,Merk,Type")] Auto auto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Autoes.Add(auto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(auto);
+        }
+
+        // GET: Autoes/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Auto auto = db.Autoes.Find(id);
+            if (auto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(auto);
+        }
+
+        // POST: Autoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Kenteken,Merk,Type")] Auto auto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(auto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(auto);
+        }
+
+        // GET: Autoes/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Auto auto = db.Autoes.Find(id);
+            if (auto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(auto);
+        }
+
+        // POST: Autoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Auto auto = db.Autoes.Find(id);
+            db.Autoes.Remove(auto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
